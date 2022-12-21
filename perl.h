758ac6294bb994f4db56000bdcc78f5a6776000a
Karl Williamson <khw@cpan.org>
2022-12-21 14:04:17 -0700

    perl.h: category_eq_ctype

--- perl.h
+++ perl.h
@@ -1425,6 +1425,19 @@ violations are fatal.
 /* end of makedef.pl logic duplication
  * ========================================================================= */
 
+#define DEBUG_SETLOCALE_INCONSISTENCIES
+#ifdef DEBUG_SETLOCALE_INCONSISTENCIES
+#  if ! defined(DEBUGGING) || defined(USE_POSIX_2008) || ! defined(USE_LOCALE_CTYPE)
+#    undef DEBUG_SETLOCALE_INCONSISTENCIES
+#  endif
+#endif
+#ifdef DEBUG_SETLOCALE_INCONSISTENCIES
+#  define PERL_ASSERT_CATEGORY_EQ_CTYPE(cat)                                \
+            assert(strEQ(setlocale(cat, NULL), setlocale(LC_CTYPE, NULL)))
+#else
+#  define PERL_ASSERT_CATEGORY_EQ_CTYPE(cat)  NOOP
+#endif
+
 #ifdef PERL_CORE
 
 /* Both typedefs are used in locale.c only, but defined here so that embed.fnc
@@ -7470,6 +7483,7 @@ the plain locale pragma without a parameter (S<C<use locale>>) is in effect.
 #ifdef LC_TIME_LOCK
 #  define STRFTIME_LOCK  /* Needs one exclusive lock */                     \
             STMT_START { LC_CTYPE_LOCK; LC_TIME_LOCK; ENV_READ_LOCK;        \
+                         PERL_ASSERT_CATEGORY_EQ_CTYPE(LC_TIME);            \
                        } STMT_END
 #  define STRFTIME_UNLOCK                                                   \
             STMT_START { ENV_READ_UNLOCK; LC_TIME_UNLOCK; LC_CTYPE_UNLOCK;  \
