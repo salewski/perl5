d8d26634460dc805aac2bf2b6b5803a66837eaa5
Karl Williamson <khw@cpan.org>
2023-01-11 16:08:02 -0700

    Rename SETLOCALE_LOCK to LOCALE_LOCK
    This mutex isn't just about the setlocale function; it applies to all
    locale related critical sections.

--- locale.c
+++ locale.c
@@ -7445,7 +7445,7 @@ Perl_my_strerror(pTHX_ const int errnum, utf8ness_t * utf8ness)
     }
     else {
 
-        SETLOCALE_LOCK;
+        LOCALE_LOCK;
 
         const char * orig_locale = toggle_locale_i(WHICH_LC_INDEX, "C");
 
@@ -7457,7 +7457,7 @@ Perl_my_strerror(pTHX_ const int errnum, utf8ness_t * utf8ness)
 
         restore_toggled_locale_i(WHICH_LC_INDEX, orig_locale);
 
-        SETLOCALE_UNLOCK;
+        LOCALE_UNLOCK;
 
         *utf8ness = UTF8NESS_IMMATERIAL;
     }
@@ -7486,7 +7486,7 @@ Perl_my_strerror(pTHX_ const int errnum, utf8ness_t * utf8ness)
                                   : "C";
     /* XXX Can fail on z/OS */
 
-    SETLOCALE_LOCK;
+    LOCALE_LOCK;
 
     LC_MESSAGES_LOCK;
     LC_CTYPE_LOCK;
@@ -7504,7 +7504,7 @@ Perl_my_strerror(pTHX_ const int errnum, utf8ness_t * utf8ness)
     restore_toggled_locale_c(LC_MESSAGES, orig_MESSAGES_locale);
     restore_toggled_locale_c(LC_CTYPE, orig_CTYPE_locale);
 
-    SETLOCALE_UNLOCK;
+    LOCALE_UNLOCK;
 
     *utf8ness = get_locale_string_utf8ness_i(errstr, LOCALE_UTF8NESS_UNKNOWN,
                                              NULL, LC_MESSAGES_INDEX_);
