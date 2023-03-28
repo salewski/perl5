e06b7bcc2e6924d9bc6429d901a367376f360f61
Karl Williamson <khw@cpan.org>
2023-03-27 18:15:43 -0600

    XXX Partial need vutil.c first Rmv underlying numeric
    This object, only on POSIX 2008 systems, is no longer used.  I've been
    keeping it around in case I could figure out if it had any remaining
    utility, but don't see any.

--- makedef.pl
+++ makedef.pl
@@ -462,7 +462,6 @@ unless ($define{USE_POSIX_2008_LOCALE})
     ++$skip{$_} foreach qw(
         PL_C_locale_obj
         PL_scratch_locale_obj
-        PL_underlying_numeric_obj
     );
 }
 unless ($define{USE_PL_CURLOCALES})
@@ -611,13 +610,6 @@ unless ($define{USE_LOCALE_THREADS} && ! $define{USE_THREAD_SAFE_LOCALE}) {
                            PL_less_dicey_locale_buf
                            PL_less_dicey_locale_bufsize
 			  );
-}
-
-
-unless ($define{USE_LOCALE_NUMERIC}) {
-    ++$skip{$_} foreach qw(
-                    PL_underlying_numeric_obj
-			 );
 }
 
 unless ($define{USE_LOCALE_CTYPE}) {
