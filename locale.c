0a1349a852d5f0744249b300c0ad4a7d82042cdc
Karl Williamson <khw@cpan.org>
2023-04-07 13:59:14 -0600

    S_bool_setlocale_2008_i: Copy function call.
    This removes a function call from a single place and inserts it in
    several.  The reason is that it makes a future commit cleaner.

--- locale.c
+++ locale.c
@@ -1964,6 +1964,11 @@ S_querylocale_2008_i(pTHX_ const unsigned int index, const line_t caller_line)
 #  define bool_setlocale_r(cat, locale)                                     \
                  bool_setlocale_i(get_category_index(cat, NULL), locale)
 
+/* If this doesn't exist on this platform, make it a no-op (to save #ifdefs) */
+#  ifndef update_PL_curlocales_i
+#    define update_PL_curlocales_i(index, new_locale)
+#  endif
+
 STATIC bool
 S_bool_setlocale_2008_i(pTHX_
 
@@ -2099,6 +2104,8 @@ S_bool_setlocale_2008_i(pTHX_
         if (! entry_obj_is_special) {
             freelocale(entry_obj);
         }
+
+        update_PL_curlocales_i(index, "C");
     }
     else {  /* Here is the general case, not to LC_ALL=>C */
         locale_t basis_obj = entry_obj;
@@ -2151,6 +2158,7 @@ S_bool_setlocale_2008_i(pTHX_
             DEBUG_NEW_OBJECT_CREATED(category_names[index], new_locale,
                                      new_obj, basis_obj, caller_line);
 
+            update_PL_curlocales_i(index, new_locale);
     }
 
 #  undef DEBUG_NEW_OBJECT_CREATED
@@ -2170,7 +2178,7 @@ S_bool_setlocale_2008_i(pTHX_
     DEBUG_Lv(PerlIO_printf(Perl_debug_log,
                            "bool_setlocale_2008_i: now using %p\n", new_obj));
 
-#ifdef MULTIPLICITY
+#  ifdef MULTIPLICITY
 
     if (PL_cur_locale_obj != new_obj) {
         DEBUG_Lv(PerlIO_printf(Perl_debug_log,
@@ -2179,13 +2187,6 @@ S_bool_setlocale_2008_i(pTHX_
                                PL_cur_locale_obj, new_obj));
         PL_cur_locale_obj = new_obj;
     }
-
-#endif
-#  ifdef USE_PL_CURLOCALES
-
-    /* We are done, except for updating our records (if the system doesn't keep
-     * them). */
-    update_PL_curlocales_i(index, new_locale);
 
 #  endif
 #  ifdef HAS_GLIBC_LC_MESSAGES_BUG
