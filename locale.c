ed7197d28c1fe5787b60c29514d9a3b5307f934a
Karl Williamson <khw@cpan.org>
2023-05-31 10:37:51 -0600

    locale.c: Workaround some libc POSIX 2008 bugs
    Some systems try to make C.UTF-8 the same locale as plain C or POSIX.
    The latter two names are synonymous according to the POSIX Standard, but
    C.UTF-8 isn't.  It might be possible to implement them all with one
    locale internally, but in some cases, at least, that isn't transparent
    to us, leading to wrong results.
    
    This commit adds a workaround for this problem.
    
    See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=269375

--- locale.c
+++ locale.c
@@ -1368,15 +1368,31 @@ S_querylocale_2008_i(pTHX_ const unsigned int index)
 
 #  ifdef USE_PL_CURLOCALES
 
-        /* PL_curlocales[] is kept up-to-date for all categories except LC_ALL,
-         * which may be invalidated by setting it to NULL, and if so, should
-         * now be calculated. */
-        if (index == LC_ALL_INDEX_ && PL_curlocales[LC_ALL_INDEX_] == NULL) {
-            PL_curlocales[LC_ALL_INDEX_] =
-                savepv(calculate_LC_ALL_string((const char **) &PL_curlocales));
+        /* If the current locale object is the C object, then the answer is "C"
+         * or POSIX, regardless of the category.  Handling this reasonably
+         * likely case specially shortcuts extra effort, and hides some bugs
+         * from us in OS's that alias other locales to C, but do so
+         * incompletely.  If our records say it is POSIX, use that; otherwise
+         * use C.  See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=269375
+         * */
+        if (cur_obj == PL_C_locale_obj) {
+            retval = mortalized_pv_copy((strEQ(PL_curlocales[index], "POSIX"))
+                                        ? "POSIX"
+                                        : "C");
         }
-
-        retval = mortalized_pv_copy(PL_curlocales[index]);
+        else if (   index == LC_ALL_INDEX_
+                 && PL_curlocales[LC_ALL_INDEX_] == NULL)
+        {
+            /* PL_curlocales[] is kept up-to-date for all categories except
+             * LC_ALL, which may be invalidated by setting it to NULL, and if
+             * so, should now be calculated.  (The function updates
+             * PL_curlocales[LC_ALL_INDEX_] ) */
+            retval = calculate_LC_ALL_string((const char **) &PL_curlocales);
+            PL_curlocales[LC_ALL_INDEX_] = savepv(retval);
+        }
+        else {
+            retval = mortalized_pv_copy(PL_curlocales[index]);
+        }
 
 #  else     /* No PL_CURLOCALES means must have some form of querylocale */
 
@@ -1397,8 +1413,26 @@ S_querylocale_2008_i(pTHX_ const unsigned int index)
 #      define querylocale(cat, cur_obj)                                     \
                               nl_langinfo_l(_NL_LOCALE_NAME(cat), cur_obj)
 #    endif
-            retval = mortalized_pv_copy(querylocale(category_masks[index],
-                                                    cur_obj));
+            retval = savepv(querylocale(category_masks[index], cur_obj));
+
+            /* querylocale() may conflate the C locale with something that
+             * isn't exactly the same.  See for example
+             * https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=269375
+             * We know that if the locale object is the C one, we
+             * are in the C locale, which may go by the name POSIX, as both, by
+             * definition, are equivalent.  But we consider any other name
+             * spurious, so override with "C".  As in the PL_CURLOCALES case
+             * above, this hides those glitches, for the most part, from the
+             * rest of our code.  (The code is ordered this way so that if the
+             * system distinugishes "C" from "POSIX", we do too.) */
+            if (cur_obj == PL_C_locale_obj && ! isNAME_C_OR_POSIX(retval)) {
+                Safefree(retval);
+                retval = savepv("C");
+            }
+
+            SAVEFREEPV(retval);
+
+
         }
 
 #  endif
